0b5e8447cc3bb4a62695b22eb3612b0e8b220207
Raymond Cabrera <rjgcengr@gmail.com>
2018-07-23 00:02:15 -0700

    Add transitionend event listeners and callback fn

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) { // safari transitionend === 'flex', but other browsers end w/ flex-grow. just check for both
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
